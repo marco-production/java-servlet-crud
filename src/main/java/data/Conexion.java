62d469d9cc421a6b2096b146df524c72ee55c1f1
Marco De la cruz <marco033F@hotmail.com>
2023-06-20 15:41:02 -0400

    First commit

---
+++ src/main/java/data/Conexion.java
@@ -0,0 +1,48 @@
+
+package data;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import org.apache.commons.dbcp2.BasicDataSource;
+
+
+public class Conexion {
+    
+    private static final String CN_STR = "jdbc:mysql://localhost:3306/java_client_crud?useSSL=false&useTimezone=true&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+    private static final String DB_USER = "root";
+    private static final String DB_PASS = "";
+    
+    // Pool de conexiones
+    public static BasicDataSource getDataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        
+        dataSource.setUrl(CN_STR);
+        dataSource.setUsername(DB_USER);
+        dataSource.setPassword(DB_PASS);
+        dataSource.setInitialSize(50);
+        
+        return dataSource;
+    }
+    
+    // Get connection
+    public static Connection getConnection() throws SQLException {
+        return getDataSource().getConnection();
+    }
+    
+    // Close ResultSet
+    public static void close(ResultSet rs) throws SQLException {
+        rs.close();
+    }
+    
+    // Close Statement
+    public static void close(Statement stmt) throws SQLException {
+        stmt.close();
+    }
+    
+    // Close Connection
+    public static void close(Connection cn) throws SQLException {
+        cn.close();
+    }
+}
